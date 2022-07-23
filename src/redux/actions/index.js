9a4a7eb2abd64c2358b13d554b5822f037568d75
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-07-23 02:25:44 -0500

    updating

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,9 @@ export const GET_CITY = "GET_CITY";
 export const GET_CITY_DETAIL = "GET_CITY_DETAIL";
 export const DELETE_CITY = "DELETE_CITY";
 
-const API_KEY = '4ae2636d8dfbdc3044bede63951a019b';
+// const API_KEY = '4ae2636d8dfbdc3044bede63951a019b';
+const API_KEY = process.env.API_KEY;
+
 
 export const getCity = (city)=>(dispatch)=>{
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
